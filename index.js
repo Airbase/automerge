25dd2148a4df927756997612f6acdfcf7fa7d7aa
mehernosh <mehernosh@users.noreply.github.com>
2021-09-18 10:46:45 +0530

    Redesign to act on auto-merge enabled, refactor, add husky + eslint

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+function shouldRun (prObj, blockedLabels, actLabel) {
+  const pullNumber = prObj.number
+  const labels = prObj.labels
+  let hasSkipLabel = false
+  let hasActLabel = false
+  for (let li = 0; li < labels.length; li++) {
+    const labelFound = labels[li].name.toLowerCase()
+    if (blockedLabels.includes(labelFound)) {
+      hasSkipLabel = true
+      console.log(`Pull #${pullNumber} has skip label: ${labelFound}!`)
+      break
+    } else if (!!actLabel && labelFound === actLabel.toLowerCase()) {
+      hasActLabel = true
+      console.log(
+        `Pull ${pullNumber} has act label: ${labelFound}, continue looking for skip labels`
+      )
+    }
+  }
+  const autoMergeEnabled = (
+    ('auto_merge' in prObj) && (!!prObj.auto_merge) && (!!prObj.auto_merge.enabled_by)
+  )
+  if (autoMergeEnabled) {
+    console.log(`Automerge enabled by: ${prObj.auto_merge.enabled_by.login}`)
+  }
+
+  return !hasSkipLabel && (
+    hasActLabel || (
+      autoMergeEnabled
+    )
+  )
+}
+
 async function base2HeadUpdate () {
   try {
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payloadStr = JSON.stringify(github.context, undefined, 2)
-    console.log(`The event payload: ${payloadStr}`)
+    // const payloadStr = JSON.stringify(github.context, undefined, 2)
+    // console.log(`The event payload: ${payloadStr}`)
 
     const repoToken = core.getInput('repo-token')
-    const workingLabel = core.getInput(
-      'act-label'
-    )
     const blockedLabels = JSON.parse(core.getInput('skip-labels')).map(v => v.toLowerCase())
+    const actLabel = core.getInput('act-label')
 
     const payload = github.context.payload
     const repoName = payload.repository.name
@@ -25,18 +55,10 @@ async function base2HeadUpdate () {
       `Operating in: ${ownerName}/${repoName}@${branchName}`
     )
 
-    if (workingLabel.length > 0) {
-      console.log(
-        `Looking for open PRs labeled with: ${workingLabel}!`
-      )
-    } else {
-      throw new Error({
-        error: "Invalid 'automerge-base2head-label'",
-        message: "Use 'with:' to specify a label to use."
-      })
-    }
-    const octokit = github.getOctokit(repoToken)
+    console.log(`Looking for open PRs to ${branchName}`)
+    console.log(`Blocked: [${blockedLabels}], include:${actLabel}`)
 
+    const octokit = github.getOctokit(repoToken)
     const pullsResponse = await octokit.rest.pulls.list(
       {
         owner: ownerName,
@@ -47,8 +69,6 @@ async function base2HeadUpdate () {
         direction: 'asc'
       }
     )
-    console.log(pullsResponse)
-
     if (!('data' in pullsResponse) || pullsResponse.data.length === 0) {
       console.log(`No pulls found pointing to branch: ${branchName}`)
       return
@@ -58,22 +78,10 @@ async function base2HeadUpdate () {
     const successes = []
 
     for (let pi = 0; pi < pullsResponse.data.length; pi++) {
-      const pullNumber = pullsResponse.data[pi].number
-      const labels = pullsResponse.data[pi].labels
-      let base2headEnabled = false
-      for (let li = 0; li < labels.length; li++) {
-        if (blockedLabels.includes(labels[li].name.toLowerCase())) {
-          base2headEnabled = false
-          const labelStr = JSON.stringify(labels[li], undefined, 4)
-          console.log(`Pull ${pullNumber} has skip label!: ${labelStr}`)
-          break
-        }
-        if (labels[li].name === workingLabel) {
-          base2headEnabled = true
-          const labelStr = JSON.stringify(labels[li], undefined, 4)
-          console.log(`Pull ${pullNumber} has label!: ${labelStr}`)
-        }
-      }
+      const prObj = pullsResponse.data[pi]
+      const pullNumber = prObj.number
+      const base2headEnabled = shouldRun(prObj, blockedLabels, actLabel)
+
       if (base2headEnabled) {
         try {
           const updateResponse = await octokit.rest.pulls.updateBranch(
@@ -83,34 +91,24 @@ async function base2HeadUpdate () {
               pull_number: pullNumber
             }
           )
-          console.log(updateResponse)
           successes.push(`#${pullNumber}`)
+          console.log(`Pull updated: ${prObj.html_url} :: ${updateResponse}`)
         } catch (e) {
-          console.log(`Failure while trying to update #${pullNumber}`)
-          console.log(typeof e)
-          console.log(e)
+          console.error(`Failure while trying to update #${pullNumber}: ${typeof e}`)
+          console.error(e)
 
-          const hasResponseMessage = (
-            ('response' in e) &&
-            ('data' in e.response) &&
-            ('message' in e.response.data)
+          const hasMergeConflicts = (
+            'response' in e && 'data' in e.response && 'message' in e.response.data &&
+            (e.response.data.message.indexOf('merge conflict') > -1)
           )
-          if (hasResponseMessage && e.response.data.message.indexOf('merge conflict') > -1) {
-            console.log(
-              `Pull #${pullNumber} has conflicts. Skipping.`
-            )
+          if (hasMergeConflicts) {
+            console.error(`Pull #${pullNumber} has conflicts. Skipping.`)
           } else {
-            failures.push(
-              {
-                pull_number: pullsResponse.data.html_url
-              }
-            )
+            failures.push({ pull_number: pullsResponse.data.html_url })
           }
         }
       } else {
-        console.log(
-          `Pull #${pullNumber} skipped.`
-        )
+        console.log(`Pull skipped: ${prObj.html_url}`)
       }
     }
 
@@ -129,9 +127,6 @@ async function base2HeadUpdate () {
 }
 
 async function run () {
-  const phases = JSON.parse(core.getInput('merge-actions'))
-  if (phases.includes('update-descendants')) {
-    await base2HeadUpdate()
-  }
+  await base2HeadUpdate()
 }
 run()
